abaa8828410e27d0c5c0d7126cf6584e0f35fbac
Jaime Flores <jaimelopesflores@gmail.com>
2021-08-09 23:23:21 +0200

    shaders introduction

--- cherno/README.md
+++ cherno/README.md
@@ -1,5 +1,4 @@
 https://www.youtube.com/playlist?list=PLlrATfBNZ98foTJPJ_Ev03o2oq3-GGOS2
 
-
 current:
-https://www.youtube.com/watch?v=x0H--CL2tUI&list=PLlrATfBNZ98foTJPJ_Ev03o2oq3-GGOS2&index=5
+https://www.youtube.com/watch?v=FBbPWSOQ0-w&list=PLlrATfBNZ98foTJPJ_Ev03o2oq3-GGOS2&index=10
--- cherno/main-triangle-shader.cpp
+++ cherno/main-triangle-shader.cpp
@@ -101,36 +101,20 @@ int main(void) {
 
   cout << glGetString(GL_VERSION) << endl;
 
-  // without vertex index buffer
-  float positions[] = {
-    -0.5f, -0.5f, // 0
-     0.5f, -0.5f, // 1
-     0.5f,  0.5F, // 2
-    -0.5f,  0.5f, // 3
-  };
-
-  // index reuse
-  unsigned int indices[] = {
-    0, 1, 2,
-    2, 3, 0
-  };
+  float positions[6] = {-0.5f, -0.5f, 0.0f, 0.5f, 0.5f, -0.5f};
 
   unsigned int buffer;
   glGenBuffers(1, &buffer);
   glBindBuffer(GL_ARRAY_BUFFER, buffer);
-  glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
   glEnableVertexAttribArray(0);
   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
-  unsigned int ibo;
-  glGenBuffers(1, &ibo);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
-
   ShaderProgramSource source = ParseShader("res/shaders/basic.shader");
 
-  unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+  unsigned int shader =
+      CreateShader(source.VertexSource, source.FragmentSource);
   glUseProgram(shader);
 
   /* Loop until the user closes the window */
@@ -138,7 +122,7 @@ int main(void) {
     /* Render here */
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     /* Swap front and back buffers */
     glfwSwapBuffers(window);
